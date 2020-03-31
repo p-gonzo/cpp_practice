d095a1d694276fb771e71aedb177013abb370d5e
Philip Gonzalez <pgonzalez115@gmail.com>
2020-03-31 15:21:55 -0600

    complete exercize

--- chp11/exercise_11_1.cpp
+++ chp11/exercise_11_1.cpp
@@ -1,22 +1,56 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 class Shape
 {
-public:
-	virtual double Area() = 0;
-	virtual void Print() = 0;
-	virtual ~Shape();
+  public:
+    virtual void Print() = 0;
+    virtual ~Shape() { };
 
-private:
-	double _area { 0 };
+  protected:
+    double _area { 0 };
 };
 
-class Circle
+class Circle: public Shape
 {
 public:
-	double Area() override
-	{
+  Circle(double _radius)
+   {
+     _area = pow(_radius * 3.14, 2);
+   }
 
-	}
+  void Print() override
+  {
+    cout << "The area of the circle is: " << _area << endl;
+  }
+};
+
+class Square: public Shape
+{
+public:
+  Square(double _side)
+  {
+    _area = _side * _side;
+  }
+  
+  void Print() override
+  {
+    cout << "The area of the square is: " << _area << endl;
+  }
+
+};
+
+
+int main()
+{
+  Circle myCircle(5);
+  myCircle.Print();
+  
+
+  Square mySquare(5);
+  mySquare.Print();
+
+  return 0;
+}
 
