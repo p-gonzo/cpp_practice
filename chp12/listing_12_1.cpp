ae3f90add80f3affea4d2cf075e78547f66a0eb1
Philip Gonzalez <Pgonzalez115@gmail.com>
2020-03-31 22:02:55 -0600

    Add initial listing for chp12

--- chp12/listing_12_1.cpp
+++ chp12/listing_12_1.cpp
@@ -1,27 +1,57 @@
 #include <iostream>
-#include <string>
-using namespace std;
 
-class Hi
+class Date
 {
-public:
-  void Print()
-  {
-    cout << "this is a location: " << this << endl;
-    cout << "(*this) dereferences: " (*this) << endl;
-  }
+private:
+    int day, month, year;
 
-  friend ostream& operator<<(ostream& os, Hi& hi)
-  {
-    os << "Hello!";
-    return os;
-  }
+public:
+    Date(int m, int d, int y)
+        : month(m), day(d), year(y) { };
+    
+    Date& operator++ () // preifx
+    {
+        ++ day;
+        return *this;
+    }
+
+    Date operator++ (int) // postfix
+    {
+        Date copy(*this);
+        ++ day;
+        return copy;
+    }
+
+    Date& operator-- ()
+    {
+        -- day;
+        return *this;
+    }
+
+    Date operator-- (int)
+    {
+        Date copy(*this);
+        -- day;
+        return copy;
+    }
+
+    friend std::ostream& operator<< (std::ostream &os, Date d)
+    {
+        os << d.month << "/" << d.day << "/" << d.year;
+        return os;
+    }
 
 };
 
 int main()
 {
-  Hi hi;
-  hi.Print();
-  return 0;
+    Date birthday (11, 19, 1988);
+
+    std::cout << "My birthday is on " << birthday << std::endl;
+
+    birthday++;
+    ++birthday;
+
+    std::cout << "Two days after my birthday is " << birthday << std::endl;
+    return 0;
 }
