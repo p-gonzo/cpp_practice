76f9551ea9cd1a36f28714fb85e485c584bc5f6c
Philip Gonzalez <pgonzalez115@gmail.com>
2020-04-15 21:54:15 -0600

    Implement rValueRef move constructor and move assignment operator

--- chp12/exercise_12_2.cpp
+++ chp12/exercise_12_2.cpp
@@ -5,9 +5,9 @@
 class DynIntegers
 {
 private:
-    int* _ints;
-    size_t _len;
-    std::string _intString;
+    int* _ints {NULL};
+    size_t _len {0};
+    std::string _intString {""};
     DynIntegers() { std::cout << "Private default constructor called" << std::endl; }
 public:
     DynIntegers(const int* input, const size_t len): _len(len)
@@ -21,24 +21,90 @@ public:
             }
         }
         else _ints = NULL;
+
+        std::ostringstream os;
+        os << "{ ";
+        if (_ints != NULL)
+        {
+            for (size_t i = 0; i < _len; ++i)
+            {
+                os << _ints[i] << ", ";
+            }
+        }
+        os << "} ";
+        _intString = os.str();
+
+        std::cout << "constructor called with " << *this << std::endl;
+    }
+
+    DynIntegers(const DynIntegers &cpySrc)
+    {
+        std::cout << "Copy constructor copies" << cpySrc << std::endl;
+        if (cpySrc._ints != NULL)
+        {
+            _len = cpySrc._len;
+            _ints = new int[_len];
+            for(size_t i = 0; i < _len; ++i)
+            {
+                _ints[i] = cpySrc._ints[i];
+            }
+        }
+        else _ints = NULL;
+    }
+
+    DynIntegers& operator=(const DynIntegers &cpySrc)
+    {
+        std::cout << "Copy assignment op. copies" << cpySrc << std::endl;
+        if (this != &cpySrc && cpySrc._ints != NULL)
+        {
+            _len = cpySrc._len;
+            _ints = new int[_len];
+            for(size_t i = 0; i < _len; ++i)
+            {
+                _ints[i] = cpySrc._ints[i];
+            }
+            _intString = cpySrc._intString;
+        }
+        else _ints = NULL;
+        return *this;
+    }
+
+    DynIntegers(DynIntegers &&mvSrc)
+    {
+        std::cout << "Move constructor moves" << mvSrc << std::endl;
+        if (mvSrc._ints != NULL)
+        {
+            _len = mvSrc._len;
+            _ints = mvSrc._ints;
+            mvSrc._len = 0;
+            mvSrc._ints = NULL;
+            mvSrc._intString = "";
+        }
+    }
+
+    DynIntegers& operator=(DynIntegers &&mvSrc)
+    {
+        std::cout << "Move assignment op moves" << mvSrc << std::endl;
+        if (mvSrc != *this && mvSrc._ints != NULL)
+        {
+            _len = mvSrc._len;
+            _ints = mvSrc._ints;
+            _intString = mvSrc._intString;
+            mvSrc._len = 0;
+            mvSrc._ints = NULL;
+            mvSrc._intString = "";
+        }
+        return *this;
     }
 
     ~DynIntegers()
     {
-        std::cout << "Destructor called" << std::endl;
+        std::cout << "Destructor called for" << *this << std::endl;
         if (_ints != NULL) delete[] _ints;
     }
 
-    operator const char*()
+    operator const char*() const
     {
-        std::ostringstream os;
-        os << "{ ";
-        for (size_t i = 0; i < _len; ++i)
-        {
-            os << _ints[i] << ", ";
-        }
-        os << "} ";
-        _intString = os.str();
         return _intString.c_str();
     }
 };
@@ -47,6 +113,9 @@ int main()
 {
     int someInts[] {1,2,3,4,5};
     DynIntegers x(someInts, 5);
-    std::cout << x <<std::endl;
+
+    int otherInts[] { 6, 7 };
+    x = DynIntegers(otherInts, 2);
+
     return 0;
 }
