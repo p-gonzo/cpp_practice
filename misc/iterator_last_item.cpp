ede11ad670eac49486e2ca3d67d86f2e3f9fc75e
Philip Gonzalez <pgonzalez115@gmail.com>
2020-05-18 10:21:34 -0600

    rm unused test code

--- misc/iterator_last_item.cpp
+++ misc/iterator_last_item.cpp
@@ -3,14 +3,8 @@
 
 int main()
 {   
-    int five = 5;
-    int ten = 10;
-    int fifteen = 15;
-    int twenty = 20;
-    int twentyFive = 25;
-    std::list<int> ints { five, ten, fifteen, twenty, twentyFive };
-    std::list<int>::iterator ptr;
-    for (ptr = ints.begin(); ptr != ints.end(); ++ptr)
+    std::list<int> ints { 5, 10, 15, 20, 25 };
+    for (auto ptr = ints.begin(); ptr != ints.end(); ++ptr)
     {
         std::cout << *ptr;
         if (std::next(ptr) != ints.end())
