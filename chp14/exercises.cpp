1002e2efe9cae3df1f181876ff627c5539277b60
Philip Gonzalez <pgonzalez115@gmail.com>
2020-06-05 18:09:59 -0600

    Attempt return dynamic pointer type based on template param

---
+++ chp14/exercises.cpp
@@ -0,0 +1,98 @@
+// #include <iostream>
+// #include <string>
+
+// #define MULTIPLY(a, b) ((a)*(b))
+
+// template <typename T1, typename T2, typename retVal>
+// retVal t_multiply(const T1 & val1, const T2 & val2)
+// {
+//     return val1 * val2;
+// }
+
+// template <typename T>
+// void swap(T &v1, T &v2)
+// {
+//     T temp = v1;
+//     v1 = v2;
+//     v2 = temp;
+// }
+
+// template <typename T1, typename T2>
+// class DoubleArrContainer
+// {
+// public:
+//     DoubleArrContainer()
+//     {
+//         static_assert(sizeof(T1) != sizeof(T2), "Error, types cannot be the same");
+//     }
+//     template <typename GetT>
+//     GetT * get()
+//     {
+//         if (sizeof(GetT) == sizeof(int))
+//         {
+//             return &(_arr1[0]);
+//         }
+//         else if (sizeof(GetT) == sizeof(double))
+//         {
+//             return &(_arr2[0]);
+//         }
+        
+//     }
+// private:
+//     T1 _arr1 [10];
+//     T2 _arr2 [10];
+// };
+
+// int main()
+// {
+//     int five {5};
+//     double twoPointOne {2.1};
+//     auto result = MULTIPLY(five + 1, twoPointOne);
+//     std::cout << result << std::endl;
+
+//     auto result2 = t_multiply<int, double, double>(five + 1, twoPointOne);
+//     std::cout << result2 <<std::endl;
+
+//     int ten {10};
+//     std::cout << "before swapping, five is: " << five << " and ten is : " << ten << std::endl;
+//     swap(five, ten);
+//     std::cout << "after swapping, five is: " << five << " and ten is: " << ten << std::endl;
+
+//     DoubleArrContainer<int, double> instance;
+//     instance.get<int>();
+
+//     return 0;
+// }
+
+#include <iostream>
+
+template <typename T1, typename T2>
+class MyClass
+{
+public:
+    MyClass(const int &a, const double &b): _a(a), _b(b) { }
+    template <typename T>
+    const T GetVal() // TODO - fails when we try to return a ptr, why?
+    {
+        if (sizeof(T) == sizeof(_a))
+        {
+            return _a;
+        }
+        else if (sizeof(T) == sizeof(_b))
+        {
+            return _b;
+        }
+    }
+private:
+    T1 _a;
+    T2 _b;
+};
+
+
+int main()
+{
+    MyClass<int, double> instance(5, 5.5);
+    std::cout << instance.GetVal<int>() << std::endl;
+    std::cout << instance.GetVal<double>() << std::endl;
+    return 0;
+}
