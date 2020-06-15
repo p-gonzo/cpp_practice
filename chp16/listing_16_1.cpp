1c448b4a7d123ff006a4e06467ac5dab7a1b00a7
Philip Gonzalez <pgonzalez115@gmail.com>
2020-06-15 16:36:49 -0600

    Play with STL string class

---
+++ chp16/listing_16_1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    const char *constCStyleString = "Hello String!\0";
+    std::cout << constCStyleString << std::endl;
+
+    std::string strFromContCharPtr ( constCStyleString );
+    std::cout << strFromContCharPtr << std::endl;
+
+    std::string strFromCpy (strFromContCharPtr);
+    std::cout << strFromCpy << std::endl;
+
+    std::string strPartialCpy (constCStyleString, 5);
+    std::cout << strPartialCpy << std::endl;
+
+    std::string otherPartialCpy (strFromCpy, 5);
+    std::cout << otherPartialCpy << std::endl;
+
+    std::string repeatedChars (10, 'a');
+    std::cout << repeatedChars << std::endl;
+
+    return 0;
+}
