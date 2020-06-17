55733a6c2854fcf9edfb8614bdf6b46577a3dbd6
Philip Gonzalez <pgonzalez115@gmail.com>
2020-06-16 21:48:02 -0600

    Implement while loop to locate all substring locations

--- chp16/listing_16_4.cpp
+++ chp16/listing_16_4.cpp
@@ -4,14 +4,26 @@
 int main()
 {
     std::string sample {"Good day string!  Today is beautiful!"};
-    size_t charPos = sample.find("day", 0);
+    
+    const char* target = "day";
+    
+    size_t charPos = sample.find(target, 0);
     if (charPos != std::string::npos)
     {
-        std::cout << "First instance of \"day\" was found at postion " << charPos << std::endl;
+        std::cout << "First instance of '" << target << "' was found at postion " << charPos << std::endl;
     }
     else
     {
-        std::cout << "Substring \"day\" was not found."  << std::endl;
+        std::cout << "Substring '" << target << "' was not found."  << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    charPos = sample.find(target, 0);
+    while (charPos != std::string::npos)
+    {
+        std::cout << "'" << target << "' found at position " << charPos << std::endl;
+        charPos = sample.find(target, ++charPos);
     }
     
     return 0;
