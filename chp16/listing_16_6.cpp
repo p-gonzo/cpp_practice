a6374894567a890b99b43e9d1af679141867a4ff
Philip Gonzalez <pgonzalez115@gmail.com>
2020-06-22 12:18:50 -0600

    Create reverse every N chars function

--- chp16/listing_16_6.cpp
+++ chp16/listing_16_6.cpp
@@ -9,9 +9,23 @@ bool isPalindrome(const std::string & str)
     return str == cpy;
 }
 
+void reverseEveryNChars(std::string & str, int N)
+{
+    // std::reverse(str.begin(), str.begin() + 3);
+    auto pos = str.begin();
+    while (pos < str.end() - N)
+    {
+        std::reverse(pos, pos + N); // up to but not including?
+        pos += N;
+    }
+}
+
 int main()
 {
-    std::string myStr {"racecar"};
+    std::string myStr {"Hello World!"};
     std::cout << myStr << " is a palindrome: " << isPalindrome(myStr) << std::endl;
+
+    reverseEveryNChars(myStr, 3);
+    std::cout  << myStr << std::endl;
     return 0;
 }
