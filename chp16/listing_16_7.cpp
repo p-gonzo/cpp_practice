9973172f8252d9ba14dd263b983cd39e54e746a6
Philip Gonzalez <pgonzalez115@gmail.com>
2020-06-23 09:23:19 -0600

    Implement custom class sorting using STL sort and lambdas

--- chp16/listing_16_7.cpp
+++ chp16/listing_16_7.cpp
@@ -1,6 +1,14 @@
 #include <algorithm>
 #include <iostream>
 #include <string>
+#include <vector>
+
+struct A
+{
+    A(int n, char c): num(n), chr(c) { };
+    int num;
+    char chr;
+};
 
 int main()
 {
@@ -14,6 +22,15 @@ int main()
     std::transform(input.begin(), input.end(), result.begin(), ::toupper);
 
     std::cout << input << " " << result << std::endl;
+
+    std::vector<A> aVec { A(5, 'g'), A( -2, 'r'), A(1, 'a') };
+
+    std::sort(aVec.begin(), aVec.end(), [](A a1, A a2) { return a1.num < a2.num; });
+
+    for (auto itr = aVec.begin(); itr < aVec.end(); ++itr)
+    {
+        std::cout << itr->num << ": " << itr->chr << std::endl;
+    }
 
     return 0;
 }
