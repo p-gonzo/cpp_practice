2bac6877546f4cb58f1ffe2c1d93b39dc5df467b
Philip Gonzalez <pgonzalez115@gmail.com>
2020-06-24 10:41:02 -0600

    Modify map to use Iterator.insert as opposed to push_back

--- misc/scratchpad.cpp
+++ misc/scratchpad.cpp
@@ -1,51 +1,40 @@
+#include <assert.h>
 #include <vector>
 #include <iostream>
 #include <string>
 #include <functional>
 #include <list>
 #include <map>
+#include <set>
+
 
 template <typename IteratorType>
 using ItemType = typename std::iterator_traits<typename IteratorType::iterator>::value_type;
 
-/* Helper Funcs*/
-
 template <typename IteratorType>
-void ForEach(IteratorType &items, std::function<void(ItemType<IteratorType> &item)> forEachCb)
+void for_each(IteratorType &items, std::function<void(ItemType<IteratorType> const &item)> forEachCb)
 {
     for (typename IteratorType::iterator ptr = items.begin(); ptr != items.end(); ++ptr)
         forEachCb(*ptr);
 }
 
-template <typename IteratorType>
-IteratorType Filter(IteratorType &items, std::function<bool(ItemType<IteratorType> &item)> filterCb)
+template <typename IteratorType, typename ReturnType>
+ReturnType transform_container(IteratorType &items, std::function<ItemType<ReturnType>(ItemType<IteratorType> const &item)> mapCb)
 {
-    IteratorType filteredIterator;
-    ForEach<IteratorType>(items, [&filteredIterator, &filterCb](auto &item) { if (filterCb(item)) filteredIterator.push_back(item); });
-    return filteredIterator;
-}
-
-template <typename IteratorType>
-IteratorType Map(IteratorType &items, std::function<ItemType<IteratorType>(ItemType<IteratorType> &item)> mapCb)
-{
-    IteratorType mappedIterator;
-    ForEach<IteratorType>(items, [&mappedIterator, &mapCb](auto &item) { mappedIterator.push_back(mapCb(item)); });
+    ReturnType mappedIterator;
+    for_each<IteratorType>(items, [&mappedIterator, &mapCb](auto &item) { mappedIterator.insert(mappedIterator.end(), mapCb(item)); });
     return mappedIterator;
-}
-
-template <typename IteratorType, typename MemoType>
-MemoType Reduce(IteratorType &items, std::function<MemoType(ItemType<IteratorType> &item, MemoType &memo)> reduceCb, const MemoType &start)
-{
-    MemoType memo = start;
-    ForEach<IteratorType>(items, [&memo, &reduceCb](auto &item) { memo = reduceCb(item, memo); });
-    return memo;
 }
 
 int main()
 {
-    std::string word {"hello"};
-    std::map<int,char> items {std::pair<int, char>(5, 'a'), std::pair<int, char>(6, 'b'), std::pair<int, char>(7, 'c')};
-    ForEach<std::map<int,char>>(items, [](std::pair<int,char> pair){ std::cout << pair.first << ": " << pair.second << std::endl; } );
-    // ForEach<std::string>(word, [](char& chr) { std::cout << chr << std::endl;});
+    std::vector<int> vi{ 1, 2, 3, 4, 5 };
+    auto vs = transform_container<std::vector<int>, std::vector<std::string>>(vi, [](int i){return std::to_string(i);});
+    assert(vs == std::vector<std::string>({"1", "2", "3", "4", "5"}));
+
+    std::set<int> si{ 5, 10, 15 };
+    auto sd = transform_container<std::set<int>, std::set<double>>(si, [] (int i) { return i / 2.; }); 
+    assert(sd == std::set<double>({5/2., 10/2., 15/2.}));
+
     return 0;
 }
