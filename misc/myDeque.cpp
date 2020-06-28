af56ea55d5500099de8e566a4c82791a94801360
Philip Gonzalez <pgonzalez115@gmail.com>
2020-06-28 11:58:42 -0600

    Add head/tail markers

--- misc/myDeque.cpp
+++ misc/myDeque.cpp
@@ -15,7 +15,8 @@ public:
     }
     void pushBack(T val)
     {
-
+        unsigned int nextIdx = _size + 1;
+        unsigned int rootIdx = 
     }
 
     T& operator[](unsigned int idx)
@@ -25,18 +26,19 @@ public:
 
 private:
     unsigned int _blockLength {8};
+    unsigned int _rootSize {1};
     unsigned int _size {0};
     unsigned int _capacity {_blockLength};
-    T** _root = new T*[1];
-
-
-
+    T* head;
+    T* tail;
+    T** _root = new T*[_rootSize];
 };
 
 int main()
 {
     MyDeque<int> ints;
-    ints.pushBack(1);
+    ints.pushBack(555);
     auto x = ints[0];
+    std::cout << x << std::endl;
     return 0;
 }
