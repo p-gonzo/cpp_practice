852fabaaa82cc79da8128a577686daa7fb6a98b7
Philip Gonzalez <pgonzalez115@gmail.com>
2020-06-28 19:11:08 -0600

    Implement pushBack with dynamic resizing

--- misc/myDeque.cpp
+++ misc/myDeque.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 template <typename T>
 class MyDeque
@@ -6,52 +7,72 @@ class MyDeque
 public:
     MyDeque()
     {
-        _root[0] = new T[_blockLength];
-        _headNode = _root[0];
-        _tailNode = _root[0];
+        _map[0] = new T[_blockLength];
+        _headBlock = _map[0];
+        _tailBlock = _map[0];
     }
 
     void pushFront(T val)
     {
+        
 
     }
     void pushBack(T val)
     {
-        // determine root and node location
-        // make new node if needed
-        unsigned int targetIdx = _size;
-        unsigned int targetNodeIndex = (unsigned int)(targetIdx/_rootSize);
+        unsigned int targetIdx = _size++;
+        unsigned int targetBlockIndex = floor(targetIdx/_blockLength);
         unsigned int targetValIndex = targetIdx % _blockLength;
-        _root[targetNodeIndex][targetValIndex] = val;
+
+        if (targetBlockIndex > _mapSize - 1)
+        {
+            unsigned int newMapSize = _mapSize * 2;
+            T** newMap = new T*[newMapSize];
+            
+            for (int i = 0; i < _mapSize; ++i)
+            {
+                newMap[i] = _map[i];
+            }
+            for (int i = _mapSize; i < newMapSize; ++i)
+            {
+                newMap[i] = new T[_blockLength];
+            }
+            delete[] _map;
+
+            _map = newMap;
+            _mapSize = newMapSize;
+            _capacity = _mapSize * _blockLength;
+        }
+        
+        std::cout << targetBlockIndex << " " << targetValIndex << std::endl;
+        _map[targetBlockIndex][targetValIndex] = val;
     }
 
     T& operator[](unsigned int idx)
     {
-        unsigned int targetNodeIndex = (unsigned int)(idx/_rootSize);
+        unsigned int targetBlockIndex = floor(idx/_blockLength);
         unsigned int targetValIndex = idx % _blockLength;
-        return _root[targetNodeIndex][targetValIndex];
+        return _map[targetBlockIndex][targetValIndex];
     }
 
 private:
     unsigned int _blockLength {8};
-    unsigned int _rootSize {1};
+    unsigned int _mapSize {1};
     
     unsigned int _size {0};
     unsigned int _capacity {_blockLength};
     
-    T** _root = new T*[_rootSize];
-    T* _headNode;
-    T* _tailNode;
+    T** _map = new T*[_mapSize];
+    T* _headBlock;
+    T* _tailBlock;
 
 };
 
 int main()
 {
     MyDeque<int> ints;
-    ints.pushBack(555);
-    auto x = ints[0];
-    std::cout << x << std::endl;
-    // unsigned int targetNodeIndex = (unsigned int)(8 / 8);
-    // std::cout << targetNodeIndex << std::endl;
-    return 0;
+    for (int i = 1; i < 11; ++i)
+    {
+        ints.pushBack(i * i);
+        std::cout << ints[i - 1] << std::endl;
+    }
 }
