d99e70db6068af7e90d67d334ed5d3d2f54555c2
Philip Gonzalez <pgonzalez115@gmail.com>
2020-06-29 17:31:11 -0600

    Implement constant time insertation at start of deque

--- misc/myDeque.cpp
+++ misc/myDeque.cpp
@@ -1,28 +1,62 @@
 #include <iostream>
 #include <cmath>
 
+struct DequeLocation
+{
+    void set(unsigned int b, unsigned int i) { block = b; idx = i; }
+    unsigned int block;
+    unsigned int idx;
+};
+
 template <typename T>
 class MyDeque
 {
 public:
     MyDeque()
     {
-        _map[0] = new T[_blockLength];
-        _headBlock = _map[0];
-        _tailBlock = _map[0];
+        _map = new T*[_mapSize];
+        for (int i = 0; i < _mapSize; ++i)
+        {
+            _map[i] = new T[_blockLength];
+        }
+        _head.set(1, 0);
+        _tail.set(1, 0);
+    }
+
+    unsigned int Size() const
+    {
+        return _size;
+    }
+
+    unsigned int Capacity() const
+    {
+        return _capacity;
     }
 
     void pushFront(T val)
     {
-        
+        _size++;
+        unsigned int targetBlockIndex = _head.block;
+        unsigned int targetValIndex = _head.idx - 1;
 
+        if (targetValIndex == -1)
+        {
+            targetValIndex = _blockLength - 1;
+            targetBlockIndex--;
+        }
+
+        _map[targetBlockIndex][targetValIndex] = val;
+
+        _head.block = targetBlockIndex;
+        _head.idx = targetValIndex;
     }
     void pushBack(T val)
     {
-        unsigned int targetIdx = _size++;
-        unsigned int targetBlockIndex = floor(targetIdx/_blockLength);
-        unsigned int targetValIndex = targetIdx % _blockLength;
+        _size++;
+        unsigned int targetBlockIndex = _tail.block;
+        unsigned int targetValIndex = _tail.idx;
 
+        // Resize
         if (targetBlockIndex > _mapSize - 1)
         {
             unsigned int newMapSize = _mapSize * 2;
@@ -43,14 +77,22 @@ public:
             _capacity = _mapSize * _blockLength;
         }
         
-        std::cout << targetBlockIndex << " " << targetValIndex << std::endl;
         _map[targetBlockIndex][targetValIndex] = val;
+        _tail.idx ++;
+        if (_tail.idx == _blockLength)
+        {
+            _tail.idx = 0;
+            _tail.block++;
+        }
+
     }
 
     T& operator[](unsigned int idx)
     {
-        unsigned int targetBlockIndex = floor(idx/_blockLength);
-        unsigned int targetValIndex = idx % _blockLength;
+        unsigned int targetBlockIndex = _head.block;
+        unsigned int targetValIndex = _head.idx + idx;
+        targetBlockIndex = targetBlockIndex + floor(targetValIndex/_blockLength);
+        targetValIndex = targetValIndex % _blockLength;
         return _map[targetBlockIndex][targetValIndex];
     }
 
@@ -65,14 +107,15 @@ public:
 
 private:
     unsigned int _blockLength {8};
-    unsigned int _mapSize {1};
+    unsigned int _mapSize {3};
     
     unsigned int _size {0};
-    unsigned int _capacity {_blockLength};
+    unsigned int _capacity {_blockLength * _mapSize};
     
-    T** _map = new T*[_mapSize];
-    T* _headBlock;
-    T* _tailBlock;
+    T** _map;
+public:
+    DequeLocation _head;
+    DequeLocation _tail;
 
 };
 
@@ -82,6 +125,14 @@ int main()
     for (int i = 1; i < 11; ++i)
     {
         ints.pushBack(i * i);
-        std::cout << ints[i - 1] << std::endl;
+    }
+    ints.pushFront(555);
+
+    std::cout << "Size: " << ints.Size() << " Capacity: " << ints.Capacity() << std::endl;
+    std::cout << "head: " << ints._head.block << " " << ints._head.idx << " tail: " << ints._tail.block << " " << ints._tail.idx << std::endl;
+
+    for (int i = 0; i < ints.Size(); ++i)
+    {
+        std::cout << ints[i] << std::endl;
     }
 }
