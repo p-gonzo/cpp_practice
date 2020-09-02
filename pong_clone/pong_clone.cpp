3e8846d96b0ba4c506a9ccb09173813dfdeea6a6
Philip Gonzalez <pgonzalez115@gmail.com>
2020-09-02 15:09:02 -0600

    flatten main function

--- pong_clone/pong_clone.cpp
+++ pong_clone/pong_clone.cpp
@@ -1,53 +1,38 @@
 //Using SDL and standard IO
 #include<SDL2/SDL.h>
+#include <iostream>
 #include <stdio.h>
 
-//Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH {640};
+const int SCREEN_HEIGHT {480};
 
 int main( int argc, char* args[] )
 {
-	//The window we'll be rendering to
-	SDL_Window* window = NULL;
-	
-	//The surface contained by the window
-	SDL_Surface* screenSurface = NULL;
+    SDL_Window* window = NULL;
+    SDL_Surface* screenSurface = NULL; //The surface contained by the window
 
-	//Initialize SDL
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-	{
-		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
-	}
-	else
-	{
-		//Create window
-		window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( window == NULL )
-		{
-			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
-		}
-		else
-		{
-			//Get window surface
-			screenSurface = SDL_GetWindowSurface( window );
+    // Attempt to init SDL
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    {
+        std::cout <<  "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+        return -1;
+    }
 
-			//Fill the surface white
-			SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0x00, 0xFF ) );
-			
-			//Update the surface
-			SDL_UpdateWindowSurface( window );
+    // Attempt to create window object
+    window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+    if( window == NULL )
+    {
+        std::cout << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        return -1;
+    }
 
-			//Wait two seconds
-			SDL_Delay( 2000 );
-		}
-	}
+    screenSurface = SDL_GetWindowSurface( window );
+    SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0x00, 0xFF ) );
+    SDL_UpdateWindowSurface( window );
+    SDL_Delay( 2000 );
 
-	//Destroy window
-	SDL_DestroyWindow( window );
+    SDL_DestroyWindow( window );
+    SDL_Quit();
 
-	//Quit SDL subsystems
-	SDL_Quit();
-
-	return 0;
+    return 0;
 }
