31a410765233b5dd44e3c1d0a7ab902fa661da16
Philip Gonzalez <pgonzalez115@gmail.com>
2020-09-02 15:51:27 -0600

    Implement SDL events to close the window and the renderer to draw the screen and net

--- pong_clone/pong_clone.cpp
+++ pong_clone/pong_clone.cpp
@@ -1,36 +1,59 @@
 //Using SDL and standard IO
-#include<SDL2/SDL.h>
+#include <SDL2/SDL.h>
 #include <iostream>
-#include <stdio.h>
 
-const int SCREEN_WIDTH {640};
-const int SCREEN_HEIGHT {480};
+const int WINDOW_WIDTH { 1280} ;
+const int WINDOW_HEIGHT { 720 };
+
+void drawNet( SDL_Renderer* renderer )
+{
+    SDL_SetRenderDrawColor( renderer, 0xFF, 0XFF, 0XFF, 0XFF );
+    for ( int y = 0; y < WINDOW_HEIGHT; ++y )
+    {
+        if ( y % 5 == 0 )
+        {
+            SDL_RenderDrawPoint( renderer, WINDOW_WIDTH / 2, y);
+        }
+    }
+}
 
 int main( int argc, char* args[] )
 {
-    SDL_Window* window = NULL;
-    SDL_Surface* screenSurface = NULL; //The surface contained by the window
-
-    // Attempt to init SDL
     if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
     {
         std::cout <<  "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
         return -1;
     }
 
-    // Attempt to create window object
-    window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+    SDL_Window* window = SDL_CreateWindow( "Pong", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN );
     if( window == NULL )
     {
         std::cout << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
         return -1;
     }
 
-    screenSurface = SDL_GetWindowSurface( window );
-    SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0x00, 0xFF ) );
-    SDL_UpdateWindowSurface( window );
-    SDL_Delay( 2000 );
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
+    bool running = true;
+
+    while ( running )
+    {
+        SDL_Event event;
+        while ( SDL_PollEvent( &event ) )
+        {
+            if ( event.type == SDL_QUIT ) running = false;
+            else if ( event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE ) running = false;
+        }
+
+        SDL_SetRenderDrawColor( renderer, 0x0, 0x0, 0x0, 0xFF );
+        SDL_RenderClear( renderer );
+
+        drawNet( renderer );
+
+        SDL_RenderPresent( renderer ); // present the "backbuffer"
+    }
+
+    SDL_DestroyRenderer( renderer );
     SDL_DestroyWindow( window );
     SDL_Quit();
 
