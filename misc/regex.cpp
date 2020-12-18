b27d59e46b839705905a2f04f9a5ee3360958a14
Philip Gonzalez <pgonzalez115@gmail.com>
2020-12-18 14:48:49 -0700

    Update regex

--- misc/regex.cpp
+++ misc/regex.cpp
@@ -5,7 +5,7 @@
 int main()
 {
     std::string prefix = "Ph";
-    std::string name = "";
+    std::string name = "Philip";
     std::regex re(prefix + "([a-zA-Z0-9]){4}");
     if(name.size() != 6 && !std::regex_match(name, re))
     {
