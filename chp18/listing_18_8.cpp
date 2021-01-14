051d4e136c03df6ec530d52d6f7bd33e952734b9
Philip Gonzalez <pgonzalez115@gmail.com>
2021-01-13 18:30:07 -0700

    Sort and transform forward_list

---
+++ chp18/listing_18_8.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <forward_list>
+#include <iostream>
+
+template <typename T>
+void DisplayContents(const T &container)
+{
+    std::cout << "---" << std::endl;
+    for (auto itr = container.cbegin(); itr != container.cend(); ++itr)
+        std::cout << *itr << std::endl;
+}
+
+int main()
+{
+    std::forward_list<int> fw{3,4,2,2,0};
+    fw.push_front(1);
+    DisplayContents(fw);
+
+    fw.remove(2);
+    fw.sort();
+    DisplayContents(fw);
+
+    std::forward_list<bool> isEven(fw., false);
+    std::transform(fw.begin(), fw.end(), isEven.begin(), [](const int &val) { return val % 2 == 0;});
+    DisplayContents(isEven);
+    return 0;
+}
